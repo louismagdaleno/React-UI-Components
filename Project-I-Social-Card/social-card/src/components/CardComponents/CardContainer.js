eacf61890e792c5d11be973894ad72a0375938a0
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-27 09:53:00 -0800

    Social-Card MVP Complete

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 const CardContainer = () => {
     return (
-        <div className="cardContainer">
+        <a href="https://www.reactjs.org"><div className="cardContainer">
             <CardBanner />
-        </div>
+            <CardContent />
+        </div></a>
         
     );
 }
