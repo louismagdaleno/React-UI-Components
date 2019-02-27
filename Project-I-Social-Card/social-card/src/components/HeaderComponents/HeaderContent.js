310e14eadd12b106da73031f57d9b5a5f8cf7651
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-26 23:50:26 -0800

    Added HeaderTitle Component to HeaderContainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import HeaderTitle from './HeaderTitle';
 import './Header.css';
 
 const HeaderContent = () => {
     return (
         
         <section>
-            <h1>Lambda School</h1>
-            <p>Let's learn react by building simple interfaces with components.  
+            <div>
+                <h1>Lambda School</h1>
+                <HeaderTitle />
+            </div>
+            
+             <p>Let's learn react by building simple interfaces with components.  
                 Don't try to overthink it, just keep it simple and have fun.
                 Once you feel comfortable using components you are well on your way
                 to mastering React!</p>
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <h4>@LambdaSchool {Date.now}</h4>
+    );
+}
+
+export default HeaderTitle;
