1b56248e9a6412709409fb834b37637e82bfe429
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-27 23:09:07 -0800

    Fixed typo in NumberButton

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -3,9 +3,9 @@ import './Button.css';
 
 const NumberButton = (props) => {
     return (
-        <div className={props.className}>
+        <button className={props.buttonStyle}>
             {props.text}
-        </div>
+        </button>
     );
 
 }
