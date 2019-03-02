9a6cfa0fbe8241f810e3c77ea9594e51084ae855
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-01 21:54:57 -0800

    more progress on calculator logic

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -10,44 +10,34 @@ class App extends React.Component {
     super(props);
     this.state = { count: '0' };
     this.total = 0;
-    this.num1 = 0;
-    this.num2 = 0;
+    this.key = '0';
   }
   
-  newNumber(amount, keyPressed) {
-    if (this.state.count === 0) {
-      this.setState({ count: amount });
-    }
-    else {
-      switch (keyPressed) {
-        case 'number':
-          if (this.state.count == '0') {
-            this.setState({ count: amount });
-          }
-          else {
-            this.setState({count: this.state.count + amount})
-          }
-          
-          
-        case '+':
-          // code block
-          this.num1 = parseInt(this.state.count);
-          this.setState({ count: this.state.count + amount });
-          break;
-        case 'clr':
-          // code block
-          this.total = 0;
-          this.num1 = 0;
-          this.num2 = 0;
-          this.setState({ count: 0 });
-          break;
-        default:
-          // code block
-        this.setState({ count: amount });
+  newNumber( keyPressed) {
+    if (this.state.count == '0' &&  ['1', '2','3','4','5','6','7','8','9'].includes(keyPressed)) {
+      this.setState({count:  keyPressed}) 
+    } else {
+      if (keyPressed == 'clear') {
+        this.total = 0;
+        this.setState({count: '0'});
+      } else if (keyPressed == '+'){
+        this.total += parseInt(this.state.count);
+        this.key = keyPressed;
+
+      } else if (keyPressed === '=') {
+        this.setState({count: this.total.toString()});
+        
+      } else {
+        if (this.key == '+') {
+          this.total += parseInt(keyPressed);
+          this.setState({count: this.total.toString()});
+        } else {
+          this.setState({count: this.state.count + keyPressed}) 
+        }
+         
       }
-      
+     
     }
-    
   }
   
   render() {
@@ -55,30 +45,30 @@ class App extends React.Component {
       <div className="calculator-wrapper">
         <CalculatorDisplay total={this.state.count} />
         <ButtonContainer className="clear-button">
-          <ActionButton className="three-text" text="clear" onClick={() => this.newNumber('0', 'clr')} />
-          <NumberButton bgColor="red-button" value="&divide;" onClick={() => this.newNumber('0', '/')}/>
+          <ActionButton className="three-text" text="clear" onClick={() => this.newNumber('clear')} />
+          <NumberButton bgColor="red-button" value="&divide;" onClick={() => this.newNumber('/')}/>
         </ButtonContainer>
         <ButtonContainer>
-          <NumberButton bgColor="num-button" value="7" onClick={() => this.newNumber('7', 'number')} />
-          <NumberButton bgColor="num-button" value="8" onClick={() => this.newNumber('8', 'number')} />
-          <NumberButton bgColor="num-button" value="9" onClick={() => this.newNumber('9', 'number')} />
-          <NumberButton bgColor="red-button" value="&times;" onClick={() => this.newNumber('0', '*')}/>
+          <NumberButton bgColor="num-button" value="7" onClick={() => this.newNumber('7')} />
+          <NumberButton bgColor="num-button" value="8" onClick={() => this.newNumber('8')} />
+          <NumberButton bgColor="num-button" value="9" onClick={() => this.newNumber('9')} />
+          <NumberButton bgColor="red-button" value="&times;" onClick={() => this.newNumber('*')}/>
         </ButtonContainer>
         <ButtonContainer>
-          <NumberButton bgColor="num-button" value="4" onClick={() => this.newNumber('4', 'number')} />
-          <NumberButton bgColor="num-button" value="5" onClick={() => this.newNumber('5', 'number')} />
-          <NumberButton bgColor="num-button" value="6" onClick={() => this.newNumber('6', 'number')} />
-          <NumberButton bgColor="red-button" value="-" onClick={() => this.newNumber('0', '-')}/>
+          <NumberButton bgColor="num-button" value="4" onClick={() => this.newNumber('4')} />
+          <NumberButton bgColor="num-button" value="5" onClick={() => this.newNumber('5')} />
+          <NumberButton bgColor="num-button" value="6" onClick={() => this.newNumber('6')} />
+          <NumberButton bgColor="red-button" value="-" onClick={() => this.newNumber('-')}/>
         </ButtonContainer>
         <ButtonContainer>
-          <NumberButton bgColor="num-button" value="1" onClick={() => this.newNumber('1', 'number')} />
-          <NumberButton bgColor="num-button" value="2" onClick={() => this.newNumber('2', 'number')} />
-          <NumberButton bgColor="num-button" value="3" onClick={() => this.newNumber('3', 'number')} />
-          <NumberButton bgColor="red-button" value="+" onClick={() => this.newNumber('0', '+')}/>
+          <NumberButton bgColor="num-button" value="1" onClick={() => this.newNumber('1')} />
+          <NumberButton bgColor="num-button" value="2" onClick={() => this.newNumber('2')} />
+          <NumberButton bgColor="num-button" value="3" onClick={() => this.newNumber('3')} />
+          <NumberButton bgColor="red-button" value="+" onClick={() => this.newNumber('+')}/>
         </ButtonContainer>
         <ButtonContainer>
           <ActionButton className="three-text" text="0" onClick={() => this.newNumber('0')} />
-          <NumberButton bgColor="red-button" value="=" onClick={() => this.newNumber('0', '=')}/>
+          <NumberButton bgColor="red-button" value="=" onClick={() => this.newNumber('=')}/>
         </ButtonContainer>
       </div>
     );
